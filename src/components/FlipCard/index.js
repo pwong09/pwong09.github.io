784e96d3311c0ba5b439701c4d4405ef293693f6
pwong09 <pwong09@gmail.com>
2022-06-13 16:40:02 -0400

    make FlipCard and ProjectsPage more responsive

--- src/components/FlipCard/index.js
+++ src/components/FlipCard/index.js
@@ -10,24 +10,29 @@ function FlipCard({
     githubLink,
     img 
 }) {
-    
-
 
     return (
         <>
             <div class="flip-card">
                 <div class="flip-card-inner">
                     <div class="flip-card-front" id={`${projectName}-front`}>
-                        <img src={img} alt={projectName} style={{ backgroundSize: 'cover'}}/>
+                        <img src={img} alt={projectName} 
+                        style={{ 
+                            width: '100%', 
+                            backgroundSize: 'center', 
+                            backgroundColor: '#fff', 
+                            maxWidth: '360px', 
+                            maxHeight: '360px'
+                        }}/>
                     </div>
                     <div class="flip-card-back" id={`${projectName}-back`}>
                         <h3>{title}</h3>
                         <p>{description}</p>
-                        <a href={projectLink} target="_blank" rel="noreferrer">
+                        <a className="project-link" href={projectLink} target="_blank" rel="noreferrer">
                             {projectName}
                         </a>
                         <br />
-                        <a href={githubLink} target="_blank" rel="noreferrer">
+                        <a className="github-link" href={githubLink} target="_blank" rel="noreferrer">
                             <FaGithub />
                         </a>
                     </div>
