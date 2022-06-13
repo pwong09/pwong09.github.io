a25e714d4aaad59d7fac8dfd4be24e4af0902aee
pwong09 <pwong09@gmail.com>
2022-06-13 08:30:02 -0400

    add jobArray to HomePage and edit styling

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -8,6 +8,7 @@ export default function Home() {
   const [letterClass, setLetterClass] = useState('text-animate');
   const nameArray = ['I','\'','m',' ','P','e','a','r','l',' ','W','o','n','g'];
   const pronounsArray = [' ','t',' h ','e','y',' ','/',' ','t','h','e','m']
+  const jobArray = [' ','F','u','l','l','-','S','t','a','c','k',' ','D','e','v','e','l','o','p','e','r']
 
   useEffect(() => {
     setTimeout(() => {
@@ -32,6 +33,13 @@ export default function Home() {
           <AnimateLetters 
             letterClass={letterClass}
             strArray={pronounsArray}
+            idx={15}
+          />
+          </h3>
+          <h3>I am a 
+          <AnimateLetters
+            letterClass={letterClass}
+            strArray={jobArray}
             idx={15}
           />
           </h3>
