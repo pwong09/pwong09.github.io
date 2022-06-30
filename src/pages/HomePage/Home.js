f78a2b641c3aa656f23ba04562ea358e1b833079
pwong09 <pwong09@gmail.com>
2022-06-29 21:14:09 -0700

    edit and adjust home welcome message

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -7,8 +7,8 @@ import Particle from '../../components/Particle';
 export default function Home() {
   const [letterClass, setLetterClass] = useState('text-animate');
   const nameArray = ['I','\'','m',' ','P','e','a','r','l',' ','W','o','n','g'];
-  const pronounsArray = [' ','t',' h ','e','y',' ','/',' ','t','h','e','m']
-  const jobArray = [' ','F','u','l','l','-','S','t','a','c','k',' ','D','e','v','e','l','o','p','e','r']
+  const pronounsArray = ['M','y',' ','p','r','o','n','o','u','n','s',' ','a','r','e',' ','t',' h ','e','y',' ','/',' ','t','h','e','m']
+  const jobArray = ['F','u','l','l','-','S','t','a','c','k',' ','D','e','v','e','l','o','p','e','r']
 
   useEffect(() => {
     setTimeout(() => {
@@ -21,27 +21,27 @@ export default function Home() {
       <div className="container home-page">
         <Particle />
         <div className="header-container">
-            <h2>Hi</h2>
-            <h1>
+            <h2>Hi,</h2>
+            <h1 className="my-name">
             <AnimateLetters 
               letterClass={letterClass}
               strArray={nameArray} 
               idx={15}
             />
           </h1>
-          <h3>my pronouns are 
-          <AnimateLetters 
-            letterClass={letterClass}
-            strArray={pronounsArray}
-            idx={15}
-          />
+          <h3>
+            <AnimateLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={15}
+            />
           </h3>
-          <h3>I am a 
-          <AnimateLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={15}
-          />
+          <h3>
+            <AnimateLetters 
+              letterClass={letterClass}
+              strArray={pronounsArray}
+              idx={15}
+            />
           </h3>
         </div>
       </div>
