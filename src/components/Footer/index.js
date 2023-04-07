96846fce03fa7569ac376c576d64fb27e9278d64
pwong09 <pwong09@gmail.com>
2023-04-07 10:46:31 -0700

    Edit Footer for accessibility

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,33 +3,33 @@ import './index.scss';
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 export default function Footer() {
-    return (
-        <footer>
-        <div className='footer-container'>
-            <section className='social-media'>
-                <div className='social-media-wrap'>
-                    <small className='website-rights'>Pearl Wong © 2022</small>
-                    <div className='social-icons'>
-                        <a
-                            className='social-icon-link'
-                            href='https://www.github.com/pwong09'
-                            target='_blank'
-                            rel="noreferrer"
-                        >
-                            <FaGithub />
-                        </a>
-                        <a
-                            className='social-icon-link'
-                            href='https://www.linkedin.com/in/pwong09'
-                            target='_blank'
-                            rel="noreferrer"
-                        >
-                            <FaLinkedin />
-                        </a>
-                    </div>
-                </div>
-            </section>
-        </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className='footer-container'>
+        <section className='social-media'>
+          <div className='social-media-wrap'>
+            <small className='website-rights'>Pearl Wong © 2022 - 2023</small>
+            <div className='social-icons'>
+              <a
+                aria-label="Github"
+                href='https://www.github.com/pwong09'
+                rel="noreferrer"
+                target='_blank'
+              >
+                <button className='social-icon-link'><FaGithub /></button>
+              </a>
+              <a
+                aria-label="LinkedIn"
+                href='https://www.linkedin.com/in/pwong09'
+                rel="noreferrer"
+                target='_blank'
+              >
+                <button className='social-icon-link'><FaLinkedin /></button>
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
+    </footer>
+  );
 }
